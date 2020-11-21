07984f1dd602eae79b5d34f3f958282503e3dd50
Aniket <aniketkumar190@gmail.com>
2020-11-21 18:46:37 +0530

    states X and 0 added

--- src/components/Square.js
+++ src/components/Square.js
@@ -4,7 +4,7 @@ import React from 'react';
 
 const Square = (prop) => {
     return (
-        <button type="button" className="square">
+        <button type="button" className="square" onClick={prop.onClick}>
             {prop.value} 
         </button>
     )
